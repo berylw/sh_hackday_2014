2f046a96157ec6c13eb6958bd71513f8143b8f30
Dutchgoose <staprans@gmail.com>
2014-08-14 15:23:19 -0700

    pushing new app up

--- CompanyQuery/CompanyQuery/urls.py
+++ CompanyQuery/CompanyQuery/urls.py
@@ -2,11 +2,10 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
-
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'CompanyQuery.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^sh_questions/', include('sh_question.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
---
+++ CompanyQuery/sh_questions/views/home.py
@@ -0,0 +1,3 @@
+def home(request, request_query=''):
+	import pdb; pdb.set_trace()
+	return render(request, 'home.html', {})
